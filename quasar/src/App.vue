887aee205d757ee899dffc547a24936b964a8cdc
yoshuu <appcs342@gmail.com>
2022-11-30 16:06:43 +0800

    新增quasar範例

--- quasar/src/App.vue
+++ quasar/src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -15,6 +21,22 @@ import TheWelcome from './components/TheWelcome.vue'
   <main>
     <TheWelcome />
   </main>
+  <div class="q-pa-md q-gutter-sm">
+    <q-avatar square color="red" text-color="white" icon="directions" />
+    <q-avatar square color="primary" text-color="white">J</q-avatar>
+    <q-avatar
+      square
+      size="100px"
+      font-size="82px"
+      color="teal"
+      text-color="white"
+      icon="directions"
+    />
+    <q-avatar square size="24px" color="orange">J</q-avatar>
+    <q-avatar square>
+      <img src="https://cdn.quasar.dev/img/avatar.png" />
+    </q-avatar>
+  </div>
 </template>
 
 <style scoped>
--- quasar/src/main.js
+++ quasar/src/main.js
@@ -1,6 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+myApp.use(Quasar, {
+  plugins: {},
+  /* ～～Quasar Config Object～～
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+  }
+  */
+});
