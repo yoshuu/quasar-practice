d868da0f7d1f2c7a1d86961a3a75cb6e8b504e34
yoshuu <appcs342@gmail.com>
2022-12-16 12:50:12 +0800

    1216 01

--- vue-ts3/src/App.vue
+++ vue-ts3/src/App.vue
@@ -1,80 +1,217 @@
-<template>
-  <div class="q-pa-md q-gutter-sm">
-    <q-btn label="Alert" color="primary" @click="alert = true" />
-    <q-btn label="Confirm" color="primary" @click="confirm = true" />
-    <q-btn label="Prompt" color="primary" @click="prompt = true" />
-
-    <q-dialog v-model="alert">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Alert</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-          repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
-          perferendis totam, ea at omnis vel numquam exercitationem aut, natus
-          minima, porro labore.
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn flat label="OK" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
-    <q-dialog v-model="confirm" persistent>
-      <q-card>
-        <q-card-section class="row items-center">
-          <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
-          <span class="q-ml-sm"
-            >You are currently not connected to any network.</span
-          >
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn flat label="Cancel" color="primary" v-close-popup />
-          <q-btn flat label="Turn on Wifi" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-
-    <q-dialog v-model="prompt" persistent>
-      <q-card style="min-width: 350px">
-        <q-card-section>
-          <div class="text-h6">Your address</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <q-input
-            dense
-            v-model="address"
-            autofocus
-            @keyup.enter="prompt = false"
-          />
-        </q-card-section>
-
-        <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Cancel" v-close-popup />
-          <q-btn flat label="Add address" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
-  </div>
-</template>
-
-<script lang="ts">
+<script setup>
 import { ref } from "vue";
 
-export default {
-  setup() {
-    return {
-      alert: ref(false),
-      confirm: ref(false),
-      prompt: ref(false),
-
-      address: ref(""),
-    };
+const apple = ref(false);
+const columns = [
+  {
+    name: "name",
+    required: true,
+    label: "Dessert (100g serving)",
+    align: "left",
+    field: (row) => row.name,
+    format: (val) => `${val}`,
+    sortable: true,
   },
-};
+  {
+    name: "calories",
+    align: "center",
+    label: "Calories",
+    field: "calories",
+    sortable: true,
+  },
+  { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
+  { name: "carbs", label: "Carbs (g)", field: "carbs" },
+  { name: "protein", label: "Protein (g)", field: "protein" },
+  { name: "sodium", label: "Sodium (mg)", field: "sodium" },
+  {
+    name: "calcium",
+    label: "Calcium (%)",
+    field: "calcium",
+    sortable: true,
+    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+  },
+  {
+    name: "iron",
+    label: "Iron (%)",
+    field: "iron",
+    sortable: true,
+    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+  },
+  {
+    name: "good",
+    label: "功能列",
+  },
+];
+
+const rows = [
+  {
+    name: "Frozen Yogurt",
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    sodium: 87,
+    calcium: "14%",
+    iron: "1%",
+  },
+  {
+    name: "Ice cream sandwich",
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    sodium: 129,
+    calcium: "8%",
+    iron: "1%",
+  },
+  {
+    name: "Eclair",
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    sodium: 337,
+    calcium: "6%",
+    iron: "7%",
+  },
+  {
+    name: "Cupcake",
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    sodium: 413,
+    calcium: "3%",
+    iron: "8%",
+  },
+  {
+    name: "Gingerbread",
+    calories: 356,
+    fat: 16.0,
+    carbs: 49,
+    protein: 3.9,
+    sodium: 327,
+    calcium: "7%",
+    iron: "16%",
+  },
+  {
+    name: "Jelly bean",
+    calories: 375,
+    fat: 0.0,
+    carbs: 94,
+    protein: 0.0,
+    sodium: 50,
+    calcium: "0%",
+    iron: "0%",
+  },
+  {
+    name: "Lollipop",
+    calories: 392,
+    fat: 0.2,
+    carbs: 98,
+    protein: 0,
+    sodium: 38,
+    calcium: "0%",
+    iron: "2%",
+  },
+  {
+    name: "Honeycomb",
+    calories: 408,
+    fat: 3.2,
+    carbs: 87,
+    protein: 6.5,
+    sodium: 562,
+    calcium: "0%",
+    iron: "45%",
+  },
+  {
+    name: "Donut",
+    calories: 452,
+    fat: 25.0,
+    carbs: 51,
+    protein: 4.9,
+    sodium: 326,
+    calcium: "2%",
+    iron: "22%",
+  },
+  {
+    name: "KitKat",
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: "12%",
+    iron: "6%",
+  },
+];
 </script>
+<template>
+  <q-dialog v-model="apple">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">Alert</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+        repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
+        perferendis totam, ea at omnis vel numquam exercitationem aut, natus
+        minima, porro labore.
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn flat label="OK" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+  <h4>凋謝了，就算無法再次綻放，只要能像火焰般迸發還是很美的</h4>
+  <div class="q-pa-md">
+    <q-table title="Treats" :rows="rows" :columns="columns" row-key="name">
+      <template #body-cell-name="props">
+        <q-td :props="props">
+          <q-btn
+            flat
+            color="primary"
+            :label="props.value"
+            @click="console.log('按下按鈕')"
+          >
+          </q-btn>
+        </q-td>
+      </template>
+      <template #body-cell-fat="props">
+        <q-td :props="props">
+          <q-btn flat color="primary" :label="props.value"> </q-btn>
+        </q-td>
+      </template>
+      <template #body-cell-good="props">
+        <q-td :props="props">
+          <q-btn
+            unelevated
+            @click="onDeleteClickAction(props.row.id)"
+            color="primary"
+            label="取消"
+            flat
+            dense
+          ></q-btn>
+          <q-btn
+            unelevated
+            @click="onDeleteClickAction(props.row.id)"
+            color="primary"
+            label="編輯"
+            flat
+            dense
+          ></q-btn>
+          <q-btn
+            unelevated
+            @click="onDeleteClickAction(props.row.id)"
+            color="primary"
+            label="賴"
+            flat
+            dense
+          ></q-btn>
+        </q-td>
+      </template>
+    </q-table>
+  </div>
+</template>
--- vue-ts3/src/main.ts
+++ vue-ts3/src/main.ts
@@ -15,6 +15,8 @@ import App from "./App.vue";
 
 const myApp = createApp(App);
 
+myApp.config.globalProperties.console = console;
+
 myApp.use(Quasar, {
   plugins: {}, // import Quasar plugins and add here
   lang: quasarLang,
