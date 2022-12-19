76ff1f37f2eafdcd4ea28afd9c45bceca44df534
yoshuu <appcs342@gmail.com>
2022-12-19 09:22:46 +0800

    1219 01

--- vue-ts3/src/App.vue
+++ vue-ts3/src/App.vue
@@ -1,8 +1,22 @@
 <script setup>
 import { ref } from "vue";
+import apple from "./apple.js";
 const selection = ref();
 </script>
 <template>
-  <q-select v-model="selection" :options="['a', 'b', 'c']">123</q-select>
+  <q-select
+    counter
+    multiple
+    style="width: 250px"
+    v-model="selection"
+    :options="apple"
+    use-chips
+    :hide-dropdown-icon="true"
+  >
+  </q-select>
 </template>
-<style scoped></style>
+<style scoped>
+.q-field__control {
+  color: red !important;
+}
+</style>
---
+++ vue-ts3/src/apple.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    label: "Panda",
+    value: "panda",
+    display: true,
+  },
+  {
+    label: "Lity",
+    value: "lity",
+  },
+  {
+    label: "Minmin",
+    value: "minmin",
+  },
+];
