caff8b86a54c6ca51907db4630a45e27a19f9652
yoshuu <appcs342@gmail.com>
2022-12-25 20:35:51 +0800

    1225 02

---
+++ vue-ts3/src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <div class="flex flex-center">
+    <q-card>
+      <q-card-section>456</q-card-section>
+    </q-card>
+  </div>
+</template>
+<style scoped></style>
