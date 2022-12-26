c7f379f9055cc378a5dbf093f283022e7c115e5c
yoshuu <appcs342@gmail.com>
2022-12-26 22:56:33 +0800

    1226 01

--- vue-ts3/src/App.vue
+++ vue-ts3/src/App.vue
@@ -5,5 +5,22 @@
       <q-card-section>456</q-card-section>
     </q-card>
   </div>
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+      <q-checkbox v-model="selection" val="teal" label="Teal" color="teal" />
+      <q-checkbox
+        v-model="selection"
+        val="orange"
+        label="Orange"
+        color="orange"
+      />
+      <q-checkbox v-model="selection" val="red" label="Red" color="red" />
+      <q-checkbox v-model="selection" val="cyan" label="Cyan" color="cyan" />
+    </div>
+
+    <div class="q-px-sm">
+      The model data: <strong>{{ selection }}</strong>
+    </div>
+  </div>
 </template>
 <style scoped></style>
